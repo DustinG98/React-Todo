fe0a9df944d05610247dcbc8e22ee965159a03d4
DustinG98 <thedustingraham@gmail.com>
2019-12-09 16:40:01 -0700

    Attempt to fix bug for production.

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  componentDidMount() {
-    localStorage.setItem("todos", JSON.stringify([]))
-  }
+  // componentDidMount() {
+  //   localStorage.setItem("todos", JSON.stringify([]))
+  // }
   constructor() {
-    
+    localStorage.setItem("todos", JSON.stringify([]))
     super();
     this.state = {
       todos: JSON.parse(localStorage.getItem("todos")),
